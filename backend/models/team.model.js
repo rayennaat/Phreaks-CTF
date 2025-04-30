cc8d6e80eeab01715f2475dac50baa34d5ff8d12
rayennaat <rayen.enaate@gmail.com>
2025-04-30 13:52:20 +0100

    modifeid team profile pic

--- backend/models/team.model.js
+++ backend/models/team.model.js
@@ -6,7 +6,7 @@ const teamSchema = new Schema({
     name: { type: String, required: true, unique: true }, // Team name, must be unique
     password: { type: String, required: true }, // Password for team management
     points: { type: Number, default: 0 }, // Points earned by the team, default is 0
-    TeamPic: { type: String, default: null }, // Optional, stores URL or path to the image
+    TeamPic: { type: String, default: 'uploads/teampicdefault.png' }, // Optional, stores URL or path to the image
     bio: { type: String, default: "" }, // A short bio for the team, optional
     link: { type: String, default: "" }, // A link for the team (e.g., website or social media), optional
     createdOn: { type: Date, default: Date.now }, // When the team was created
